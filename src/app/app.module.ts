e084f8aca456fdb69961d43393c8481af1191bf3
Milan Ferus-Comelo <51026229+mferuscomelo@users.noreply.github.com>
2021-09-05 14:18:47 +0200

    Add profile menu dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,10 +21,13 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { ContentLayoutComponent } from './layouts/content-layout/content-layout.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HeaderComponent } from './core/header/header.component';
 import { NavComponent } from './core/nav/nav.component';
 import { FooterComponent } from './core/footer/footer.component';
+
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/compat/auth';
 import { USE_EMULATOR as USE_DATABASE_EMULATOR } from '@angular/fire/compat/database';
@@ -49,29 +52,12 @@ import { USE_EMULATOR as USE_FIRESTORE_EMULATOR } from '@angular/fire/compat/fir
     AngularFireAuthModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatMenuModule,
   ],
   providers: [
     ScreenTrackingService,
     UserTrackingService,
     PerformanceMonitoringService,
-    {
-      provide: USE_AUTH_EMULATOR,
-      useValue: environment.useEmulators
-        ? ['http://localhost', 9099]
-        : undefined,
-    },
-    {
-      provide: USE_DATABASE_EMULATOR,
-      useValue: environment.useEmulators
-        ? ['http://localhost', 9000]
-        : undefined,
-    },
-    {
-      provide: USE_FIRESTORE_EMULATOR,
-      useValue: environment.useEmulators
-        ? ['http://localhost', 8080]
-        : undefined,
-    },
   ],
   bootstrap: [AppComponent],
 })
