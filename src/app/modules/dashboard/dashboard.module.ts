94963039d2567d673b78faf22f12518352398ccd
Milan Ferus-Comelo <51026229+mferuscomelo@users.noreply.github.com>
2021-09-06 02:02:31 +0200

    Update temperature card

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -6,14 +6,18 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+import { GetLastItemPipe } from 'src/app/shared/pipes/get-last-item.pipe';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, GetLastItemPipe],
   imports: [
     CommonModule,
     DashboardRoutingModule,
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
   ],
 })
 export class DashboardModule {}
--- src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { last } from 'rxjs/operators';
+import { DBResponse } from 'src/app/shared/models/db.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  temperatureList: Observable<DBResponse[]>;
+  // temperature: Observable<DBResponse>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private db: AngularFireDatabase) {
+    this.temperatureList = db.list<DBResponse>('temperature').valueChanges();
+    // this.temperature = this.temperatureList.pipe(
+    //   last()
+    // ) as Observable<DBResponse>;
   }
 
+  ngOnInit(): void {
+    this.temperatureList.subscribe((list) => {
+      console.log(list);
+    });
+  }
 }
