16a1dfd699ccb2006e729e994c9fb1ac2e9925a7
Milan Ferus-Comelo <51026229+mferuscomelo@users.noreply.github.com>
2021-12-11 12:11:37 +0100

    Enlarged weather card

--- src/app/core/services/weather.service.ts
+++ src/app/core/services/weather.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { Hourly, WeatherData } from 'src/app/shared/models/weather-data.model';
+import {
+  Daily,
+  Hourly,
+  WeatherData,
+} from 'src/app/shared/models/weather-data.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -9,6 +13,8 @@ import { environment } from 'src/environments/environment';
 })
 export class WeatherService {
   currentWeather: Observable<Hourly> | undefined;
+  hourlyWeather: Observable<Hourly[]> | undefined;
+  dailyWeather: Observable<Daily[]> | undefined;
 
   constructor(private http: HttpClient) {
     this.getWeather();
@@ -18,19 +24,24 @@ export class WeatherService {
   }
 
   private getWeather() {
-    const excludeParts = 'current,minutely,daily,alerts';
+    const excludeParts = 'current,minutely,alerts';
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=48.805563&lon=9.213399&exclude=${excludeParts}&appid=${environment.openWeatherApiKey}&units=metric`;
 
     this.http.get<WeatherData>(url).subscribe((data) => {
       const unixTime = Math.round(new Date().getTime() / 1000);
 
-      this.currentWeather = of(
-        data.hourly.reduce(function (prev, curr) {
-          return Math.abs(curr.dt - unixTime) < Math.abs(prev.dt - unixTime)
-            ? curr
-            : prev;
-        })
-      );
+      // TODO: Only select if time has passed
+      const current = data.hourly.reduce(function (prev, curr) {
+        return Math.abs(curr.dt - unixTime) < Math.abs(prev.dt - unixTime)
+          ? curr
+          : prev;
+      });
+
+      const index = data.hourly.indexOf(current);
+      // TODO: limit to 24 hours
+      this.hourlyWeather = of(data.hourly.slice(index + 1));
+      this.currentWeather = of(current);
+      this.dailyWeather = of(data.daily.slice(1, 3));
     });
   }
 }
--- src/app/shared/models/weather-data.model.ts
+++ src/app/shared/models/weather-data.model.ts
@@ -6,6 +6,67 @@ export interface WeatherData {
   timezone: string;
   timezone_offset: number;
   hourly: Hourly[];
+  daily: Daily[];
+}
+
+export interface Daily {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  temp: Temp;
+  feels_like: FeelsLike;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: Weather[];
+  clouds: number;
+  pop: number;
+  uvi: number;
+  rain?: number;
+}
+
+export interface FeelsLike {
+  day: number;
+  night: number;
+  eve: number;
+  morn: number;
+}
+
+export interface Temp {
+  day: number;
+  min: number;
+  max: number;
+  night: number;
+  eve: number;
+  morn: number;
+}
+
+export interface Weather {
+  id: number;
+  main: Main;
+  description: Description;
+  icon: string;
+}
+
+export enum Description {
+  BrokenClouds = 'broken clouds',
+  ClearSky = 'clear sky',
+  FewClouds = 'few clouds',
+  LightRain = 'light rain',
+  OvercastClouds = 'overcast clouds',
+  ScatteredClouds = 'scattered clouds',
+}
+
+export enum Main {
+  Clear = 'Clear',
+  Clouds = 'Clouds',
+  Rain = 'Rain',
 }
 
 export interface Hourly {
@@ -23,11 +84,4 @@ export interface Hourly {
   wind_gust: number;
   weather: Weather[];
   pop: number;
-}
-
-export interface Weather {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
 }
